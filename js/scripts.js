4cc6f400f5f17f43e599a7d515829b9b35c14325
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-02-06 13:48:44 +0100

    Add list of first 5 pokémons with basic details

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+let pokemonList = [];
+
+pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass','poison'],
+    category: 'seed'
+  },
+  {
+    name: 'Ivysaur',
+    height: 1,
+    weight: 13,
+    type: ['grass','poison'],
+    category: 'seed'
+  },
+  {
+    name: 'Venusaur',
+    height: 2,
+    weight: 100,
+    type: ['grass','poison'],
+    category: 'seed'
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire'],
+    category: 'lizard'
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    weight: 19,
+    type: ['fire'],
+    category: 'flame'
+  }
+];
+
+console.log(pokemonList);
