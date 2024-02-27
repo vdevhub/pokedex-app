d4198f30f41a60fc678680c07dfb55746fa975bd
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-02-27 13:11:10 +0100

    Add basic Pokemon modal

--- js/scripts.js
+++ js/scripts.js
@@ -99,9 +99,65 @@ let pokemonRepository = (function () {
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      showModal(pokemon.name, pokemon.height)
     });
-    //console.log(pokemon.name);
   }
+
+
+
+  //Shows the modal for Pokemon
+  function showModal(title, text) {
+    let modalContainer = document.querySelector('#pokemon-modal-container');
+
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('pokemon-modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('pokemon-modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    })
+  }
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#pokemon-modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#pokemon-modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+
+
+
+
 
   //Gets all stored Pokemons
   function getAll() {
